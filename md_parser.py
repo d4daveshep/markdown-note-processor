2227a42c611be3099dca672b6f944e7d9fa02681
d4daveshep <david@daveshep.net.nz>
2025-04-01 22:08:45 +1300

    added constructor to class and trying to get pytest working from neovim

---
+++ md_parser.py
@@ -0,0 +1,12 @@
+from enum import StrEnum
+from pathlib import Path
+
+
+class Heading(StrEnum):
+    H1 = "H1"
+    H2 = "H2"
+
+
+class MDParser:
+    def __init__(self, filepath: Path):
+        pass
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,16 @@
 from pathlib import Path
 
+from md_parser import Heading, MDParser
 
-def test_load_file():
-    md_filepath: Path = Path("Test Week 1.md")
+
+def test_load_file() -> None:
+    md_filepath: Path = Path("tests/Test Week 1.md")
     assert md_filepath.exists()
+
+
+def test_find_h1_text() -> None:
+    md_filepath: Path = Path("tests/Test Week 1.md")
+    md_parser: MDParser = MDParser(md_filepath)
+
+    h1: str = md_parser.find_next(heading=Heading.H1)
+    assert h1
