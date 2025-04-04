69c6ae8a133c4f911e2019f6404c0cb7a67fd4cd
d4daveshep <david@daveshep.net.nz>
2025-04-05 09:01:59 +1300

    added more tests, valid and invalid

--- tests/test_file_validation.py
+++ tests/test_file_validation.py
@@ -2,5 +2,19 @@ from md_parser import NoteFile
 
 
 def test_first_h1_is_weekly_format():
-    good_h1: str = "# W12 2025: 3 Nov - 9 Nov"
-    assert NoteFile.validate_weekly_heading(good_h1)
+    valid_h1: list[str] = [
+        "# W01 2023: This is a valid string",
+        "# W52 2024: Another valid string",
+    ]
+    for heading in valid_h1:
+        assert NoteFile.validate_weekly_heading(heading)
+
+    invalid_h1: list[str] = [
+        "# W00 2023: Invalid - week number out of range",
+        "# W53 2023: Invalid - week number out of range",
+        "# W12 23: Invalid - year isn't 4 digits",
+        " # W12 2023: Invalid - doesn't start at beginning of string",
+        "# w12 2023: Invalid - 'W' must be uppercase",
+    ]
+    for heading in invalid_h1:
+        assert not NoteFile.validate_weekly_heading(heading)
