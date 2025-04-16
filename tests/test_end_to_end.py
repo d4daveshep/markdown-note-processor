7a4f7d3f31788981583a8c6e86a4213de472542c
d4daveshep <david@daveshep.net.nz>
2025-04-16 21:02:23 +1200

    added marker to start next time

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -6,6 +6,7 @@ from md_parser import NoteFile, SplitResults
 
 def test_split_first_weekly_file(week_1: NoteFile, temp_dir: Path) -> None:
     results: SplitResults = week_1.split_file()
+    assert results.lines_processed == 54
 
     # check project files are created and are written as expected
     project_files: list[str] = [f"Project {n}.md" for n in range(0, 3)]
@@ -14,8 +15,6 @@ def test_split_first_weekly_file(week_1: NoteFile, temp_dir: Path) -> None:
         assert filecmp.cmp(Path(f"./tests/{file}"), Path(temp_dir / file)), (
             f"{file} differs"
         )
-
-    assert results.lines_processed == 54
 
 
 def test_split_file_dry_run(week_1: NoteFile, temp_dir: Path) -> None:
@@ -31,3 +30,6 @@ def test_split_file_dry_run(week_1: NoteFile, temp_dir: Path) -> None:
 def test_split_second_weekly_file(week_2: NoteFile, temp_dir: Path) -> None:
     results: SplitResults = week_2.split_file()
     assert results.lines_processed == 38
+    assert False, (
+        "add some test functionality here,  e.g. are the files correct?  which got amended vs created"
+    )
