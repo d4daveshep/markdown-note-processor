414d1bea7d95445f64cf7b49dabc25762e87b22c
d4daveshep <david@daveshep.net.nz>
2025-04-25 17:03:02 +1200

    added & tested ProjectFileHeadings class

--- project_file_utils.py
+++ project_file_utils.py
@@ -2,11 +2,11 @@ from pathlib import Path
 from md_parser import Heading
 
 
-def read_file_to_str_list(file: Path) -> list[str]:
+def read_file_to_str_list(filepath: Path) -> list[str]:
     """
     Read a file into a list of strings.
     """
-    with open(file, "r") as file:
+    with open(filepath, "r") as file:
         data: str = file.read()
         return data.split("\n")
 
@@ -18,3 +18,22 @@ def get_h2_headings(file: Path) -> set[str]:
     lines: list[str] = read_file_to_str_list(file)
 
     return {line for line in lines if line.startswith(Heading.H2)}
+
+
+class ProjectFileHeadings:
+    def __init__(self, directory: Path) -> None:
+        self._cache: dict[str, set[str]] = {}
+        self.directory = directory
+        self.read_project_files(directory)
+
+    def read_project_files(self, directory: Path) -> int:
+        self._cache = {}
+        for md_file in directory.glob("*.md"):
+            self._cache[md_file.stem] = get_h2_headings(md_file)
+        return len(self._cache)
+
+    def contains(self, project_filename: str, h2_heading: str) -> bool:
+        try:
+            return h2_heading in self._cache[project_filename]
+        except KeyError:
+            return False
--- tests/test_project_file_utils.py
+++ tests/test_project_file_utils.py
@@ -1,9 +1,13 @@
 from pathlib import Path
-from project_file_utils import read_file_to_str_list, get_h2_headings
+from project_file_utils import (
+    read_file_to_str_list,
+    get_h2_headings,
+    ProjectFileHeadings,
+)
 
 
 def test_read_file_to_str_list(project_file_1: Path):
-    lines: list[str] = read_file_to_str_list(file=project_file_1)
+    lines: list[str] = read_file_to_str_list(filepath=project_file_1)
     assert len(lines) == 33
 
 
@@ -11,8 +15,33 @@ def test_project_file_h2_headings(project_file_1: Path):
     """
     Test we can get the set of h2 headings in a project file
     """
-    h2_headings: list[str] = get_h2_headings(project_file_1)
+    h2_headings: set[str] = get_h2_headings(project_file_1)
     assert len(h2_headings) == 5
 
     h2_heading: str = "## Mon 08 Jan 2025: Second week on this project"
     assert h2_heading in h2_headings
+
+
+def test_project_files_h2_headings_class():
+    project_file_directory: Path = Path("tests/week_2_files/")
+    project_file_headings: ProjectFileHeadings = ProjectFileHeadings(
+        project_file_directory
+    )
+
+    assert len(project_file_headings._cache) == 7
+    assert "Project 3" in project_file_headings._cache
+    assert len(project_file_headings._cache["Project 3"]) == 3
+
+    assert project_file_headings.contains(
+        project_filename="Project 1",
+        h2_heading="## Mon 08 Jan 2025: Second week on this project",
+    )
+
+    assert not project_file_headings.contains(
+        project_filename="Non-existent file", h2_heading=""
+    )
+
+    assert not project_file_headings.contains(
+        project_filename="Project 1",
+        h2_heading="## No such heading",
+    )
