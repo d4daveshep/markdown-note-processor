a135b761d2a0efe9d206682e0977efb0727bae4d
d4daveshep <david@daveshep.net.nz>
2025-04-27 20:16:00 +1200

    moving results classes to separate file

--- md_parser.py
+++ md_parser.py
@@ -1,14 +1,14 @@
 import argparse
 import logging
 import re
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from datetime import datetime
 from io import TextIOWrapper
 from pathlib import Path
 from typing import NamedTuple
 
 from project_file_utils import ProjectFileHeadings, read_file_to_str_list, Heading
-
+from split_results import SplitResults, TitleDate, ProjectFileDetails
 
 logging.basicConfig(format="%(asctime)s %(message)s")
 log = logging.getLogger(__name__)
@@ -34,52 +34,52 @@ class SplitState:
     title: str = ""
 
 
-class TitleDate(NamedTuple):
-    title: str = ""
-    date_str: str = ""
+# class TitleDate(NamedTuple):
+#     title: str = ""
+#     date_str: str = ""
 
 
-@dataclass
-class ProjectFileDetails:
-    name: str
-    created: bool = False
-    lines_written: dict[TitleDate, int] = field(default_factory=dict[TitleDate, int])
+# @dataclass
+# class ProjectFileDetails:
+#     name: str
+#     created: bool = False
+#     lines_written: dict[TitleDate, int] = field(default_factory=dict[TitleDate, int])
 
 
-@dataclass
-class SplitResults:
-    lines_processed: int = 0
-    week_num: str = ""
-    top_heading: str = ""
-    projects: dict[str, ProjectFileDetails] = field(
-        default_factory=dict[str, ProjectFileDetails]
-    )
-    days: set[str] = field(default_factory=set[str])
-
-    def __str__(self) -> str:
-        output = ""
-        output += "\n======== Split Results ========\n"
-        output += self.top_heading
-        output += "\n-------------------------------\n"
-        output += f"Processed {self.lines_processed} lines\n"
-
-        output += "Project files written to:\n"
-        for project_name, details in sorted(self.projects.items()):
-            output += f"- {project_name}\t"
-            if details.created:
-                output += "[ CREATED ]\n"
-            else:
-                output += "[ exists ]\n"
-        output += "-------------------------------\n"
-
-        output += "\nNotes entries written:\n"
-        for project_name, details in sorted(self.projects.items()):
-            output += f"\nProject file: {project_name}:\n"
-            for title_date, lines in details.lines_written.items():
-                output += f'On {title_date[1]}, topic "{title_date[0]}", wrote {lines} lines\n'
-        output += "-------------------------------\n"
-
-        return output
+# @dataclass
+# class SplitResults:
+#     lines_processed: int = 0
+#     week_num: str = ""
+#     top_heading: str = ""
+#     projects: dict[str, ProjectFileDetails] = field(
+#         default_factory=dict[str, ProjectFileDetails]
+#     )
+#     days: set[str] = field(default_factory=set[str])
+#
+#     def __str__(self) -> str:
+#         output = ""
+#         output += "\n======== Split Results ========\n"
+#         output += self.top_heading
+#         output += "\n-------------------------------\n"
+#         output += f"Processed {self.lines_processed} lines\n"
+#
+#         output += "Project files written to:\n"
+#         for project_name, details in sorted(self.projects.items()):
+#             output += f"- {project_name}\t"
+#             if details.created:
+#                 output += "[ CREATED ]\n"
+#             else:
+#                 output += "[ exists ]\n"
+#         output += "-------------------------------\n"
+#
+#         output += "\nNotes entries written:\n"
+#         for project_name, details in sorted(self.projects.items()):
+#             output += f"\nProject file: {project_name}:\n"
+#             for title_date, lines in details.lines_written.items():
+#                 output += f'On {title_date[1]}, topic "{title_date[0]}", wrote {lines} lines\n'
+#         output += "-------------------------------\n"
+#
+#         return output
 
 
 class NoteFile:
---
+++ split_results.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass, field
+from typing import NamedTuple
+
+
+class TitleDate(NamedTuple):
+    title: str = ""
+    date_str: str = ""
+
+
+@dataclass
+class ProjectFileDetails:
+    name: str
+    created: bool = False
+    lines_written: dict[TitleDate, int] = field(default_factory=dict[TitleDate, int])
+
+
+@dataclass
+class SplitResults:
+    lines_processed: int = 0
+    week_num: str = ""
+    top_heading: str = ""
+    projects: dict[str, ProjectFileDetails] = field(
+        default_factory=dict[str, ProjectFileDetails]
+    )
+    days: set[str] = field(default_factory=set[str])
+
+    def __str__(self) -> str:
+        output = ""
+        output += "\n======== Split Results ========\n"
+        output += self.top_heading
+        output += "\n-------------------------------\n"
+        output += f"Processed {self.lines_processed} lines\n"
+
+        output += "Project files written to:\n"
+        for project_name, details in sorted(self.projects.items()):
+            output += f"- {project_name}\t"
+            if details.created:
+                output += "[ CREATED ]\n"
+            else:
+                output += "[ exists ]\n"
+        output += "-------------------------------\n"
+
+        output += "\nNotes entries written:\n"
+        for project_name, details in sorted(self.projects.items()):
+            output += f"\nProject file: {project_name}:\n"
+            for title_date, lines in details.lines_written.items():
+                output += f'On {title_date[1]}, topic "{title_date[0]}", wrote {lines} lines\n'
+        output += "-------------------------------\n"
+
+        return output
