843ecea111fe0dc5218047d2ec1474bb8a7fb68c
d4daveshep <david@daveshep.net.nz>
2025-04-28 20:53:45 +1200

    specify signature for write_projectfile function

--- tests/test_projectfile_writer.py
+++ tests/test_projectfile_writer.py
@@ -1,5 +1,6 @@
 from projectfile_writer import write_project_files
 from split_results import SplitResults
+from tests.conftest import temp_dir
 from weekly_notes import WeeklyNotes
 
 
@@ -11,3 +12,7 @@ def test_write_project_files(week_1_notes: WeeklyNotes):
 def test_write_project_file(week_1_notes: WeeklyNotes):
     h1_heading = week_1_notes.h1_headings[2]
     h2_heading = h1_heading.h2_headings[2]
+
+    projectfile_details: ProjectFileDetails = write_projectfile(
+        h2_heading, temp_dir, projectfile_headings
+    )
