393b4e82829979aeb8b862d2ea00b6d79e039efb
d4daveshep <david@daveshep.net.nz>
2025-04-30 17:33:06 +1200

    refactor away from using md_parser package

--- projectfile_writer.py
+++ projectfile_writer.py
@@ -1,15 +1,86 @@
-from io import TextIOWrapper
 import logging
-from md_parser import NoteFile, ProjectFileDetails
+import re
+from datetime import datetime
+from io import TextIOWrapper
 from pathlib import Path
 
+# from md_parser import ProjectFileDetails
 from project_file_utils import ProjectFileHeadings
-from split_results import SplitResults, TitleDate
+from split_results import SplitResults, TitleDate, ProjectFileDetails
 from weekly_notes import H2Heading, WeeklyNotes
 
 logging.basicConfig(format="%(asctime)s %(message)s")
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
+
+
+class FormatException(Exception):
+    pass
+
+
+def validate_weekly_heading(h1_heading: str) -> str:
+    """
+    Validate the heading matches the correct weekly format: # Week nn yyyy: <date range>
+
+    Rules are as follows:
+        nn must be two digits e.g. 01, 45
+        yyyy is the year
+        <date range> is not validated and could be any text
+
+    Return: the week string e.g. "Week 12 2024"
+    """
+    pattern: str = r"^# Week\s(0[1-9]|[1-4][0-9]|5[0-2])\s+\d{4}:"
+
+    if bool(re.match(pattern, h1_heading)):
+        log.debug(f'Week_num="{h1_heading[2:14]}"')
+        return h1_heading[2:14]
+    else:
+        raise FormatException(
+            'Invalid format on line 1, expecting "# Week nn yyyy: ..."'
+        )
+
+
+def validate_date_heading(h1_heading: str) -> str:
+    """
+    Validate the H1 heading is a valid date in the format Mon 6 Jan 2025.
+    Raise: FormatException if format is invalid, note the day of week is not verified to be correct
+    Return: the date in same format it was specified
+    """
+    if not h1_heading.startswith("# ") or len(h1_heading) < 3:
+        raise FormatException('Invalid date format, expecting "# ddd dd mmm yyyy"')
+    date_str: str = h1_heading[2:]
+    try:
+        datetime.strptime(date_str, "%a %d %b %Y")
+        log.debug(f'Date_str="{date_str}"')
+    except ValueError:
+        log.debug(
+            f'Invalid date format: "{h1_heading}" - Ignoring lines until valid date found'
+        )
+        date_str = ""
+    return date_str
+
+
+def split_project_name_heading(h2_heading: str) -> tuple[str, str]:
+    """
+    Split the H2 heading into project name and title around the first hyphen in the heading
+
+    Return: project_name, title
+    """
+    project_name: str
+    title: str
+    if not h2_heading.startswith("## ") or len(h2_heading) < 4:
+        raise FormatException('Invalid format, expecting "## <project name>"')
+    hyphen_index: int = h2_heading.find("-", 3)
+    if hyphen_index == -1:
+        project_name = h2_heading[3:]
+        title = ""
+    else:
+        project_name = h2_heading[3:hyphen_index]
+        title = h2_heading[hyphen_index + 1 :]
+    if project_name == "":
+        raise FormatException('Invalid format, expecting "## <project name>"')
+    log.debug(f'Project_name="{project_name}", Title="{title}"')
+    return project_name.strip(), title.strip()
 
 
 def write_project_file(
@@ -30,7 +101,7 @@ def write_project_file(
     )
 
     # extract the project name and title
-    project_name, title = NoteFile.split_project_name_heading(h2_heading.name)
+    project_name, title = split_project_name_heading(h2_heading.name)
     log.debug(f"project name = {project_name}, title = {title}")
 
     # create the project file details
@@ -98,7 +169,7 @@ def write_project_files(
     for h1_heading_num, h1_heading in enumerate(weekly_notes.h1_headings):
         # write the "week-long" project notes
         if h1_heading_num == 0:
-            NoteFile.validate_weekly_heading(h1_heading.name)
+            validate_weekly_heading(h1_heading.name)
             weekly_date_str: str = h1_heading.name[2:]
             results.week_num = weekly_date_str
 
@@ -113,7 +184,7 @@ def write_project_files(
 
         # write the project files for all daily notes
         else:
-            date_str: str = NoteFile.validate_date_heading(h1_heading.name)
+            date_str: str = validate_date_heading(h1_heading.name)
 
             # write to the project files for "week-long" projects
             for h2_heading in h1_heading.h2_headings:
