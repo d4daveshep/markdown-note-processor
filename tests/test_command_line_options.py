8b7d3d2c16a5d3f1227de17ace00f923fe607262
d4daveshep <david@daveshep.net.nz>
2025-05-01 10:53:17 +1200

    add output_dir to command line args test

--- tests/test_command_line_options.py
+++ tests/test_command_line_options.py
@@ -1,8 +1,9 @@
-from md_parser import CommandLineArguments, parse_args
+from projectfile_writer import CommandLineArguments, parse_args
 
 
 def test_dry_run_argument() -> None:
-    args: CommandLineArguments = parse_args(["file.md", "--dry-run"])
+    args: CommandLineArguments = parse_args(["file.md", "-o", "temp/", "--dry-run"])
     assert args.filename == "file.md"
+    assert args.output_dir == "temp/"
     assert args.dry_run
     assert not args.debug
