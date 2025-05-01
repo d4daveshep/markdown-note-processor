3ac04dbae597e2b1b26a82d9ad559854400e6608
d4daveshep <david@daveshep.net.nz>
2025-05-01 12:06:04 +1200

    more tidyups before merge to main

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from markdown-note-processor!")
-
-
-if __name__ == "__main__":
-    main()
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ from typing import Generator
 
 import pytest
 
-from md_parser import NoteFile
 from notefile_reader import load_weekly_note_file
 from weekly_notes import WeeklyNotes
 
@@ -27,36 +26,6 @@ def temp_dir() -> Generator[Path]:
     # for file_path in test_dir.glob("*"):
     #     if file_path.is_file():
     #         file_path.unlink()
-
-
-@pytest.fixture
-def week_1(temp_dir: Path) -> Generator[NoteFile]:
-    # specify the file and the directory to work in
-    src_md_filepath: Path = Path("tests/week_1_files/Test Week 1.md")
-
-    # copy the weekly file to test directory
-    md_filepath: Path = temp_dir / src_md_filepath.name
-    shutil.copy(src_md_filepath, md_filepath)
-
-    # load the file
-    note_file: NoteFile = NoteFile(md_filepath)
-
-    yield note_file
-
-
-@pytest.fixture
-def week_2(temp_dir: Path) -> Generator[NoteFile]:
-    # specify the file and the directory to work in
-    src_md_filepath: Path = Path("tests/week_2_files/Test Week 2.md")
-
-    # copy the weekly file to test directory
-    md_filepath: Path = temp_dir / src_md_filepath.name
-    shutil.copy(src_md_filepath, md_filepath)
-
-    # load the file
-    note_file: NoteFile = NoteFile(md_filepath)
-
-    yield note_file
 
 
 @pytest.fixture
